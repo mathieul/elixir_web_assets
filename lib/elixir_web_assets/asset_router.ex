1c4c8e802fd50a631f5e0670c4f14458e41053d9
Mathieu Lajugie <mathieu@caring.com>
2014-01-19 17:13:09 -0800

    update default config

--- lib/elixir_web_assets/asset_router.ex
+++ lib/elixir_web_assets/asset_router.ex
@@ -10,9 +10,8 @@ defmodule ElixirWebAssets.AssetRouter do
   end
 
   get "/javascripts/*" do
-    bundle = !conn.params[:body]
-    minify = !!conn.params[:min]
-    content = AssetPipeline.script_content filename(conn), [bundle: bundle, minify: minify]
+    options = [bundle: !conn.params[:body], minify: !!conn.params[:min]]
+    content = AssetPipeline.script_content filename(conn), options
     conn.resp 200, content
   end
 
--- lib/elixir_web_assets/supervisor.ex
+++ lib/elixir_web_assets/supervisor.ex
@@ -13,15 +13,32 @@ defmodule ElixirWebAssets.Supervisor do
   end
 
   defp asset_pipeline_config do
-    config = Mix.project[:elixir_web_assets] || Keyword.new
     [
-      script_path: Keyword.get(config, :script_path, Path.expand("assets/javascripts")),
-      script_load_paths: Keyword.get(config, :script_load_paths, []),
-      stylesheet_path: Keyword.get(config, :stylesheet_path, Path.expand("assets/stylesheets")),
-      stylesheet_load_paths: Keyword.get(config, :stylesheet_load_paths, []),
+      script_path: default_script_path,
+      script_load_paths: default_script_load_paths,
+      stylesheet_path: default_stylesheet_path,
+      stylesheet_load_paths: default_stylesheet_load_paths,
       libs: Keyword.get(config, :libs, []),
       debug: Keyword.get(config, :debug, false)
     ]
+  end
+
+  defp config, do: Mix.project[:elixir_web_assets] || Keyword.new
+
+  defp default_script_path do
+    Keyword.get config, :script_path, Path.expand("assets/javascripts")
+  end
+
+  defp default_script_load_paths do
+    Keyword.get config, :script_load_paths, [ Path.expand("bower_components") ]
+  end
+
+  defp default_stylesheet_path do
+    Keyword.get config, :stylesheet_path, Path.expand("assets/stylesheets")
+  end
+
+  defp default_stylesheet_load_paths do
+    Keyword.get config, :stylesheet_load_paths, []
   end
 
 end
