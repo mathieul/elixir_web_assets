a1fa22cca2f0d23e35d574ce678261933596c02d
Mathieu Lajugie <mathieu@caring.com>
2014-01-21 17:38:11 -0800

    use pattern matching to access private record

--- lib/elixir_web_assets/asset_pipeline.ex
+++ lib/elixir_web_assets/asset_pipeline.ex
@@ -12,49 +12,56 @@ defmodule ElixirWebAssets.AssetPipeline do
   end
 
   defcall set_script_path(path), state: state do
-    case CommandWrapper.set_script_path(state_rec(state, :port), path) do
+    state_rec(port: port) = state
+    case CommandWrapper.set_script_path(port, path) do
       :ok   -> set_and_reply state, :ok
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall set_stylesheet_path(path), state: state do
-    case CommandWrapper.set_stylesheet_path(state_rec(state, :port), path) do
+    state_rec(port: port) = state
+    case CommandWrapper.set_stylesheet_path(port, path) do
       :ok   -> set_and_reply state, :ok
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall add_script_load_path(path), state: state do
-    case CommandWrapper.add_script_load_path(state_rec(state, :port), path) do
+    state_rec(port: port) = state
+    case CommandWrapper.add_script_load_path(port, path) do
       :ok   -> set_and_reply state, :ok
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall add_stylesheet_load_path(path), state: state do
-    case CommandWrapper.add_stylesheet_load_path(state_rec(state, :port), path) do
+    state_rec(port: port) = state
+    case CommandWrapper.add_stylesheet_load_path(port, path) do
       :ok   -> set_and_reply state, :ok
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall script_filenames(filename), state: state do
-    case CommandWrapper.script_filenames(state_rec(state, :port), filename) do
+    state_rec(port: port) = state
+    case CommandWrapper.script_filenames(port, filename) do
       { :filenames, filenames } -> set_and_reply state, tuple_to_list(filenames)
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall script_digest_filename(filename), state: state do
-    case CommandWrapper.script_digest_filename(state_rec(state, :port), filename) do
+    state_rec(port: port) = state
+    case CommandWrapper.script_digest_filename(port, filename) do
       { :filename, filename } -> set_and_reply state, filename
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall script_content(filename, options), state: state do
-    case CommandWrapper.script_content(state_rec(state, :port), filename, options) do
+    state_rec(port: port) = state
+    case CommandWrapper.script_content(port, filename, options) do
       { :content, content } -> set_and_reply state, content
       error -> set_and_reply state, { :error, error }
     end
@@ -62,14 +69,16 @@ defmodule ElixirWebAssets.AssetPipeline do
   def script_content(filename), do: script_content(filename, [])
 
   defcall stylesheet_digest_filename(filename), state: state do
-    case CommandWrapper.stylesheet_digest_filename(state_rec(state, :port), filename) do
+    state_rec(port: port) = state
+    case CommandWrapper.stylesheet_digest_filename(port, filename) do
       { :filename, filename } -> set_and_reply state, filename
       error -> set_and_reply state, { :error, error }
     end
   end
 
   defcall stylesheet_content(filename, options), state: state do
-    case CommandWrapper.stylesheet_content(state_rec(state, :port), filename, options) do
+    state_rec(port: port) = state
+    case CommandWrapper.stylesheet_content(port, filename, options) do
       { :content, content } -> set_and_reply state, content
       error -> set_and_reply state, { :error, error }
     end
@@ -80,8 +89,8 @@ defmodule ElixirWebAssets.AssetPipeline do
     { :stop, :normal, state }
   end
 
-  def terminate(_reason, state) do
-    CommandWrapper.stop state_rec(state, :port)
+  def terminate(_reason, state_rec(port: port)) do
+    CommandWrapper.stop port
     :ok
   end
 
